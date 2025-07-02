b279ac3ac3ccbc2bc7c4cc8fbb61eaeaeeedb884
pankaj1132 <pankaj17469@gmail.com>
2025-07-02 15:05:20 +0530

    project changes

--- quadbytetech/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header({ title, subtitle }) {
-  return (
-    <div className="py-7 text-white">
-      <h1 className="text-5xl font-bold text-green-400">{title}</h1>
-      <p className="mt-4 text-lg text-gray-300 max-w-xl">{subtitle}</p>
-    </div>
-  );
-}
